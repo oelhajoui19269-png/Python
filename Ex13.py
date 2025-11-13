d6ad8615785ffe61e4b7de45a44519bc785efcef
[200~ <oelhajoui19269@iesjoanramis.org>
2025-11-13 12:06:31 +0100

    Ex13.py

---
+++ Ex13.py
@@ -0,0 +1,12 @@
+opcio=int(imput("""Elegeix una opcio:
+                1. Suma
+                2. Resta
+                3. Muliplicació
+                4. Divisió
+                0. Sotir
+                """))
+a = int(imput("Escriue el primer operand: "))
+b = int(imput("Escriure el segon mnombre operand: "))
+if opcio==1:
+    Print("La suma de {} + {} és {}" .format(a, b, a+b)
+elif opcio==2:
