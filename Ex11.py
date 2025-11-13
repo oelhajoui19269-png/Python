1e44b4da5d88432c7e2b6a66cf8c6856b834bb0a
[200~ <oelhajoui19269@iesjoanramis.org>
2025-11-13 11:41:52 +0100

    Ex11

---
+++ Ex11.py
@@ -0,0 +1,3 @@
+nom = input ("inrrodueix el seu nom: ")
+Cognom = input ("introdueix el seu cognom: ")
+Print("hola, {} {}" .format (nom,cognom))
---
+++ prova1.py
@@ -0,0 +1,4 @@
+x = int(input("Escriure el primer nombre: "))
+y = int(input("Escriure el primer nombre: "))
+z = x + y
+print("El resultat de la suma {} + {} és {}".format(x, y, z))     
