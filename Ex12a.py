2e91bacb747155ceb773a7ea1ad2bcea280f14fe
[200~ <oelhajoui19269@iesjoanramis.org>
2025-11-18 11:45:49 +0100

    Ex12a.py

---
+++ Ex12a.py
@@ -0,0 +1,12 @@
+Edat = int(input("Escriu la seva edat:")
+if Edat > 18
+    print("Es major d'edat.")
+elif edat <18:
+    print("no es major d'edat.")
+else:
+    print("te exactament 18 anys.")
+          
+Edat = int(input("Escriu la seva edat:")
+majoredat(edat)
+Edat = int(input("Escriu la seva edat:")
+majoredat(edat)
